addff78e7b27959707925260e4c2f46dd9894e85
KrishikaRaj <129051277+KrishikaRaj@users.noreply.github.com>
2023-05-05 18:17:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
    public static void main(String[] args)
     {
-        System.out.println("Thank You Deepak");
+        System.out.println("Thank You Deepak For helpping me");
     }
 }

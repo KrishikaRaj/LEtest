c5746bf221fc471dfb7e90a73f6f699a6183527e
KrishikaRaj <129051277+KrishikaRaj@users.noreply.github.com>
2023-05-05 18:09:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
    public static void main(String[] args)
     {
-        System.out.println("Hello I am in Local Git Repository");
+        System.out.println("Thank You Deepak");
     }
 }
